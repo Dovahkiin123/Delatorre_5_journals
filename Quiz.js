b8520660cc08a660f4f3add1f16242264c2544a9
Anthony De la Torre <Dovahkiin123@users.noreply.github.com>
2016-03-02 13:14:43 -0600

    Added files via upload

---
+++ Quiz.js
@@ -0,0 +1,22 @@
+var answer1;
+var answer2;
+var answer3;
+var answer4;
+var answer5;
+var answer6;
+var answer7;
+var answer8;
+var answer9;
+var answer10;
+var a1 = false;
+var a2 = false;
+var a3 = false;
+var a4 = false;
+var a5 = false;
+var a6 = false;
+var a7 = false;
+var a8 = false;
+var a9 = false;
+var a10 = false;
+
+window.alert ("
